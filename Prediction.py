51569e98eeceb198240092fceb033032e55016ca
Ghedo44 <federicoghedini@gmail.com>
2021-02-01 21:17:11 +0100

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -148,3 +148,46 @@ plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, sta
 plt.axis('equal')
 plt.savefig('Subreddit=' + source + '_Keyword=' + keyword + '_Date=' + now[:10] + '_Sentiment' + '.png')
 plt.show()
+
+# Salvo i dati su un excel
+from openpyxl import Workbook, load_workbook
+
+# re-open and append
+try:
+  wb = load_workbook(keyword + '_SentimentOverTime.xlsx')
+  ws = wb.active
+  #ws.delete_rows(idx=5)
+  ws.append([now[:10], pos, neg, pos+neg, pos/(pos+neg)])
+  wb.save(keyword + '_SentimentOverTime.xlsx')
+except:
+  # create the file
+  wb = Workbook()
+  ws = wb.active
+  ws.append(['Date', 'Pos', 'Neg', 'Tot', 'Perc'])
+  ws.append([now[:10], pos, neg, pos+neg, pos/(pos+neg)])
+  wb.save(keyword + '_SentimentOverTime.xlsx')
+  
+  # Con i dati salvati sull'excel creo un grafico del sentimento 
+xls_file = pd.ExcelFile(keyword + '_SentimentOverTime.xlsx')
+
+df4 = xls_file.parse()
+print(df4.head())
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mtick
+import numpy as np
+
+df4['Date'] = df4['Date'].astype(str)
+df4['Perc'] = df4['Perc']*100
+
+# Some example data to display
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.plot(df4['Date'], df4['Perc'], color = "g", marker = "o", linestyle= "--", linewidth = 3, markersize = 10)
+ax1.yaxis.set_major_formatter(mtick.PercentFormatter())
+ax1.set_title('% di commenti positivi')
+ax2.plot(df4['Date'], df4['Tot'], color = "g", marker = "o", linestyle= "--", linewidth = 3, markersize = 10)
+ax2.set_title('Numero di commenti')
+fig.autofmt_xdate()
+plt.savefig('Subreddit=' + source + '_Keyword=' + keyword + '_SentimentOvertTime' + '.png')
+plt.show()
