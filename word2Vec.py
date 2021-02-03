9170ce5b309c93e452c8775f1ad69fae27d7a8e9
Ghedo44 <federicoghedini@gmail.com>
2021-02-03 10:31:16 +0100

    Test Word2Vec

---
+++ word2Vec.py
@@ -0,0 +1,46 @@
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+
+scrapped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence')
+article = scrapped_data .read()
+
+parsed_article = bs.BeautifulSoup(article,'lxml')
+
+paragraphs = parsed_article.find_all('p')
+
+article_text = ""
+
+for p in paragraphs:
+    article_text += p.text
+
+# Cleaing the text
+processed_article = article_text.lower()
+processed_article = re.sub('[^a-zA-Z]', ' ', processed_article)
+processed_article = re.sub(r'\s+', ' ', processed_article)
+
+# Preparing the dataset
+all_sentences = nltk.sent_tokenize(processed_article)
+
+all_words = [nltk.word_tokenize(sent) for sent in all_sentences]
+
+# Removing Stop Words
+from nltk.corpus import stopwords
+for i in range(len(all_words)):
+    all_words[i] = [w for w in all_words[i] if w not in stopwords.words('english')]
+
+#Creating word2vecmodel
+from gensim.models import Word2Vec
+
+word2vec = Word2Vec(all_words, min_count=2)
+
+vocabulary = word2vec.wv.vocab
+print(vocabulary)
+
+v1 = word2vec.wv['artificial']
+
+sim_words = word2vec.wv.most_similar('intelligence')
+
+print(sim_words)
+print(v1)
